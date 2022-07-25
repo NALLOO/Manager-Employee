8066a0da7bbbcea3eeb95e35a7f7f191a7bf81ee
NALLOO <nghiak57dl1@gmail.com>
2022-07-25 15:59:02 +0700

    2

--- src/view/PopUpForm.vue
+++ src/view/PopUpForm.vue
@@ -43,7 +43,7 @@
                 <div>
 
                     <div class="input">
-                        <label for="">Birth date</label>
+                        <label for="">Birth date r</label>
                         <input  v-model="employee.birth_date" type="date">
                         
                     </div>
